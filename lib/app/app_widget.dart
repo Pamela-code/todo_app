01d5121ff601526db0c0a34bc7d60d0554dd6822
Pamela-code <pamela.souza-jpa@hotmail.com>
2023-05-25 12:24:54 -0300

    feat: add tabbar and tabbar view

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -7,6 +7,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Todo App',
       theme: ThemeData(primarySwatch: Colors.blue),
       routeInformationParser: Modular.routeInformationParser,
--- lib/app/modules/todos/pages/todos_page.dart
+++ lib/app/modules/todos/pages/todos_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/colors.dart';
+
 class TodosPage extends StatefulWidget {
   const TodosPage({super.key});
 
@@ -10,6 +12,55 @@ class TodosPage extends StatefulWidget {
 class _TodosPageState extends State<TodosPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: primaryColor,
+          title: const Text('To do list'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Container(
+                height: 40,
+                decoration: const BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      30,
+                    ),
+                  ),
+                ),
+                child: TabBar(
+                  tabs: const [
+                    Text('All'),
+                    Text('pending'),
+                    Text('done'),
+                  ],
+                  indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: primaryColor,
+                  ),
+                ),
+              ),
+              const Expanded(
+                child: TabBarView(children: [
+                  Text('1'),
+                  Text('2'),
+                  Text('3'),
+                ]),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
