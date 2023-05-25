2c68ee31434d7c83f6ed3dc879f2a64e9bee1336
Pamela-code <pamela.souza-jpa@hotmail.com>
2023-05-25 20:04:36 -0300

    feat: add colors

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Colors.blue;
+const primaryColor = Color(0XFF023262);
+const secondaryColor = Color(0XFF1365B9);
+const backgroundColor = Color(0XFFF3FFFF);
+const grayTextColor = Color(0XFFDFE9F2);
+const gray1Color = Color(0XFF8E9698);
+const blackTextColor = Colors.black;
